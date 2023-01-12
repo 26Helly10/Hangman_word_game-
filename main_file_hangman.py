5a4130a1d3ec183dffa41c48d4f9d2fb46d471ac
Elena <53377385+26Helly10@users.noreply.github.com>
2023-01-13 00:01:46 +0200

    Update main_file_hangman.py

--- main_file_hangman.py
+++ main_file_hangman.py
@@ -1,1 +1,17 @@
-print("test")
+import random
+
+list_of_words=["abruptly", "absurd", "lengths", "syndrome", "thumbscrew","galaxy", "pixel","nightclub", "unknown" ]
+
+#the_random_word= random.choice(list_of_words)
+#splitting the random word into letters and inserting the letters into a list 
+list_of_char=[*random.choice(list_of_words)]
+for i in range(0, len(list_of_char)):
+    print("x ")
+lives=6
+number_of_l=len(list_of_char)
+print(number_of_l)
+print("you have 6 lives")
+letter=input("insert a letter: ")
+while lives>0 and number_of_l>0:
+    print("de continuat")
+print(list_of_char)
