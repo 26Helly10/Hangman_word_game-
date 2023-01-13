b50e84745c05ad5f8f759bb433cae0498854f333
Elena <53377385+26Helly10@users.noreply.github.com>
2023-01-13 17:17:30 +0200

    Update main_file_hangman.py

--- main_file_hangman.py
+++ main_file_hangman.py
@@ -5,13 +5,36 @@ list_of_words=["abruptly", "absurd", "lengths", "syndrome", "thumbscrew","galaxy
 #the_random_word= random.choice(list_of_words)
 #splitting the random word into letters and inserting the letters into a list 
 list_of_char=[*random.choice(list_of_words)]
+list_of_x=[]
 for i in range(0, len(list_of_char)):
-    print("x ")
+    list_of_x.append("_")
+
+#initial lives
 lives=6
+#number of letters 
 number_of_l=len(list_of_char)
-print(number_of_l)
-print("you have 6 lives")
-letter=input("insert a letter: ")
-while lives>0 and number_of_l>0:
-    print("de continuat")
-print(list_of_char)
+#initial number of g
+letter_g=0
+
+while lives>0 and number_of_l!=0:
+    print(f"{list_of_x}")
+    print(f"you have {lives} lives")
+    letter=input("insert a letter: ")
+    ckeck_exist=number_of_l
+    actual_number_of_l= number_of_l
+    for i in range(0,len(list_of_char)):
+        if letter ==list_of_char[i]:
+            list_of_x[i]=letter
+            number_of_l-=1  
+        else:
+            ckeck_exist-=1
+    print(ckeck_exist)
+    print(actual_number_of_l)
+    if ckeck_exist <= 0:
+        lives-=1
+    #print("de continuat")
+    #lives-=1
+if lives == 0:
+    print(f"You lose: {list_of_char}")
+elif number_of_l==0:
+    print(f"You win: {list_of_char}")
