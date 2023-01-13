fa834cc40d6bcf8018beb3bb85cd4f28b4ff040b
Elena <53377385+26Helly10@users.noreply.github.com>
2023-01-13 17:41:15 +0200

    Update main_file_hangman.py

--- main_file_hangman.py
+++ main_file_hangman.py
@@ -11,6 +11,64 @@ for i in range(0, len(list_of_char)):
 
 #initial lives
 lives=6
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+nr1_stage=6
+stages_game=stages[nr1_stage]
 #number of letters 
 number_of_l=len(list_of_char)
 #initial number of g
@@ -19,6 +77,7 @@ letter_g=0
 while lives>0 and number_of_l!=0:
     print(f"{list_of_x}")
     print(f"you have {lives} lives")
+    print(f"{stages_game}")
     letter=input("insert a letter: ")
     ckeck_exist=number_of_l
     actual_number_of_l= number_of_l
@@ -28,11 +87,15 @@ while lives>0 and number_of_l!=0:
             number_of_l-=1  
         else:
             ckeck_exist-=1
-    print(ckeck_exist)
-    print(actual_number_of_l)
+    #print(ckeck_exist)
+    #print(actual_number_of_l)
     if ckeck_exist <= 0:
+        nr1_stage-=1
+        print(f"{nr1_stage}")
+        stages_game=stages[nr1_stage]
         lives-=1
 if lives == 0:
+    print(f"{stages_game}")
     print(f"You lose: {list_of_char}")
 elif number_of_l==0:
     print(f"You win: {list_of_char}")
